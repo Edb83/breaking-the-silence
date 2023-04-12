6818be8b589d62fd1e12e00f49ca7fe1aee5331c
Edb83 <62900492+Edb83@users.noreply.github.com>
2023-04-12 21:58:34 +0100

    Add base.html, base.css and add staticfiles settings; Add home app with home view and template for testing; Add django-allauth

--- breaking_silence/settings.py
+++ breaking_silence/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +60,10 @@ ROOT_URLCONF = 'breaking_silence.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR/'templates'),
+            os.path.join(BASE_DIR/'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
