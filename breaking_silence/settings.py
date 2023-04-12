0914ba04ce4fe35ff286e7175a2601e889b554d6
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-04-12 21:47:25 +0000

    add env.py

--- breaking_silence/settings.py
+++ breaking_silence/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ]
+ALLOWED_HOSTS = ['https://8000-edb83-breakingthesilen-q69f245kenw.ws-eu93.gitpod.io/', 'localhost', ]
 
 
 # Application definition
