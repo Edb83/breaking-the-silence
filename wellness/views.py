5cd4305235adda511d02b7feabbda4017a7f54e7
BenD2525 <bendawson2525@gmail.com>
2023-04-13 19:01:20 +0000

    Fix bug with user_check_in view

--- wellness/views.py
+++ wellness/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from .models import Wellness
 from .forms import CheckinForm
 
+@login_required
 def user_check_in(request):
     if request.method == 'POST':
         form = CheckinForm(request.POST)
         if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/wellness/')
+            wellness = form.save(commit=False)
+            wellness.user = request.user
+            wellness.save()
+            return HttpResponseRedirect('/wellness/tracker')
     else:
         form = CheckinForm()
-    return render(request, 'checkin.html', {'form': form})
+    return render(request, 'wellness/checkin.html', {'form': form})
+
