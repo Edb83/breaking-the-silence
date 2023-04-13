df5822ad572983b03cf6bbabdec3a3d6a9137c7f
Edb83 <62900492+Edb83@users.noreply.github.com>
2023-04-14 00:26:13 +0100

    Add create_connection view

--- connections/urls.py
+++ connections/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('handle/<int:connection_id>/<str:decision>', views.handle_connection, name='handle_connection'),
+    path('create/<str:username>/', views.create_connection, name='create_connection'),
+    path('handle/<int:connection_id>/<str:decision>/', views.handle_connection, name='handle_connection'),
 ]
--- connections/views.py
+++ connections/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import ConnectionRequest
 
+
+
+@login_required
 def handle_connection(request, connection_id, decision):
     connection_request = get_object_or_404(ConnectionRequest, pk=connection_id)
     if decision == 'accept':
@@ -11,3 +16,19 @@ def handle_connection(request, connection_id, decision):
 
     return redirect('user_profile', request.user.username)
 
+
+@login_required
+def create_connection(request, username):
+    receiver = get_object_or_404(User, username=username)
+    connection, created = ConnectionRequest.objects.get_or_create(
+        sender=request.user,
+        receiver=receiver,
+    )
+    if created:
+        # messages sent
+        pass
+    else:
+        # messages already sent
+        pass
+
+    return redirect('user_profile', request.user.username)
