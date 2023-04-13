6b41851033fa0759809245a28fd3d7cd32b873c4
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-04-13 22:02:03 +0000

    added forum functinality, views, models, urls so users can coment on topics and coment on comnts.

--- breaking_silence/urls.py
+++ breaking_silence/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('profiles/', include('profiles.urls')),
+    path('forum/', include('forum.urls')),
 ]
--- forum/models.py
+++ forum/models.py
@@ -6,6 +6,8 @@ from django.utils import timezone
 
 class Topic(models.Model):
     title = models.CharField(max_length=255)
+    content = models.TextField(null=True, blank=True)
+    is_private = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
 
@@ -19,4 +21,13 @@ class Post(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.content[:50] + "..."
+        return self.content[:50]
+
+class Comment(models.Model):
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:50]
