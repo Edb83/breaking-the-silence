07c8f89e5324ccc085bc4fa770dcd034fde0e30a
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2023-04-13 15:47:29 +0000

    add redirect to imports on profile views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
