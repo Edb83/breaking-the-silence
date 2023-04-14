c27fc5c0a89b54e213f2cecb1730a67bdc3abbc2
Edb83 <62900492+Edb83@users.noreply.github.com>
2023-04-15 00:14:40 +0100

    Add Conversation and Message models

--- connections/admin.py
+++ connections/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Connection
+from .models import Connection, Conversation, Message
+
+class MessageAdminInline(admin.TabularInline):
+    model = Message
+
+
+class ConversationAdmin(admin.ModelAdmin):
+    inlines = (MessageAdminInline, )
+
 
 admin.site.register(Connection)
+admin.site.register(Conversation, ConversationAdmin)
--- connections/models.py
+++ connections/models.py
@@ -20,3 +20,26 @@ class Connection(models.Model):
 
     def __str__(self):
         return f'{self.sender} -> {self.receiver}'
+
+
+class Conversation(models.Model):
+    connection = models.ForeignKey(Connection, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Conversation {self.id}'
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Message {self.id} from {self.sender}'
+
+    class Meta:
+        ordering = ['created_at']
+
+
